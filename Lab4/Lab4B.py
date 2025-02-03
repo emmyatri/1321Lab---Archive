c40acabb180968479607d968a9d9c34f935b9671
emmyatri <amelia.ellingson@gmail.com>
2025-02-02 20:39:42 -0500

    updating submission for lab
    Lab4A,B,C

--- Lab4/Lab4B.py
+++ Lab4/Lab4B.py
@@ -4,3 +4,44 @@
 #Instructor: Roshni Satish
 #Name: Amelia Ellingson
 #Lab#: Lab4B
+
+#welcome message + input
+print("Welcome!")
+user_number = float(input("Please input a number: "))
+user_number2 = str(user_number)
+
+#display numbers menu
+print("What would you like to do with this number:")
+print("0) Get the additive inverse of the number")
+print("1) Get the reciprocal of the number")
+print("2) Square the number")
+print("3) Cube the number")
+print("4) Exit the program")
+
+operation = int(input())
+
+match operation:
+
+    case 0:
+    # additive inverse
+        result = str(user_number * -1)
+        print(f"The additive inverse of "+ user_number2 + " is " + result)
+    case 1:
+        #reciprocal (1/n)
+        if user_number == 0:
+            print("Cannot divide by 0!")
+        else:
+            result = str(round (1 / user_number,3))
+            print(f"The reciprocal of " + user_number2 + " is " + result)
+    case 2:
+        # square the number
+            result = str(round(user_number ** 2, 2))
+            print(f"The square of " + user_number2 + " is " + result)
+    case 3:
+        # cube the number
+        result = str(round(user_number ** 3, 2))
+        print(f"The cube of " + user_number2 + " is " + result)
+    case 4:
+        print("Thank you, goodbye!")
+    case _:
+        print("Invalid option!")
--- Lab4/Lab4C.py
+++ Lab4/Lab4C.py
@@ -4,3 +4,27 @@
 #Instructor: Roshni Satish
 #Name: Amelia Ellingson
 #Lab#: Lab4C
+
+side1 = int(input("Enter the first side of the triangle: "))
+side2 = int(input("Enter the second side of the triangle: "))
+side3 = int(input("Enter the third side of the triangle: "))
+
+#sides must be greater than 0
+
+if side1 > 0 and side2 > 0 and side3 > 0:
+    if (side1 + side2 > side3) and (side2 + side3 > side1) and (side1 + side3 > side2):
+        if side1 == side2 == side3:
+            print("The triangle is an equilateral triangle.")
+        else:
+            if side1 == side2 or side1 == side3 or side2 == side3:
+                print("The triangle is an isosceles triangle.")
+            else:
+                print("The triangle is a scalene triangle.")
+    else:
+        print("The sides do not form a valid triangle.")
+else:
+    print("Invalid input. All sides must be greater than 0.")
+
+
+
+
