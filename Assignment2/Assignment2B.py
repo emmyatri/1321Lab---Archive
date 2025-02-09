cad7f44f8f096afea4c4b44858a35296e8ecaa78
emmyatri <amelia.ellingson@gmail.com>
2025-02-09 16:55:07 -0500

    update assignment 2b

--- Assignment2/Assignment2B.py
+++ Assignment2/Assignment2B.py
@@ -16,31 +16,31 @@ creditscore = int(input("Enter your credit score: "))
 #credit score validation
 if creditscore < 300 or creditscore > 850:
         print("Invalid credit score. Score must be between 300 and 850.")
+        exit()
 
 #age validation
 else:
     if age < 18:
         print("You do not qualify for a loan due to age.")
+        exit()
 
 #credit score match case
-    else:
-        match creditscore:
-            case creditscore if 700 <= creditscore <= 850:
-                creditscore = "Good"
-            case creditscore if 600 <= creditscore <= 699:
-                creditscore = "Fair"
-            case creditscore if 300 <= creditscore <= 599:
-                creditscore = "Poor"
+
+match creditscore:
+    case creditscore if 700 <= creditscore <= 850:
+        creditscore = "Good"
+    case creditscore if 600 <= creditscore <= 699:
+        creditscore = "Fair"
+    case creditscore if 300 <= creditscore <= 599:
+        creditscore = "Poor"
 
 #automatic denial
 if creditscore == "Poor":
     print("You do not qualify for a loan due to poor credit.")
-
 #income to credit score match case and approval
 else:
     if income >=100000 and creditscore == "Good":
         print("You qualify for a Premium Loan.")
-
     elif income >= 50000 < 100000:
         if creditscore == "Good" or creditscore == "Fair":
             print("You qualify for a Standard Loan")
