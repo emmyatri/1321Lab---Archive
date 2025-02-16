e38de27122f5a3d430519e0f97e13415c7472824
emmyatri <amelia.ellingson@gmail.com>
2025-02-16 15:25:24 -0500

    Update Lab6A.py

--- Lab6/Lab6A.py
+++ Lab6/Lab6A.py
@@ -4,3 +4,59 @@
 #Instructor: Roshni Satish
 #Name: Amelia Ellingson
 #Lab#: Lab6A
+
+def multiplication(a, b) -> int:
+    result = 0
+    for _ in range(b):
+        result += (a)
+    return result
+
+def exponentiate(base, exponent) -> int:
+    if exponent == 0:
+        return 1
+    result = 1
+    for _ in range(exponent):
+        result = 0
+        for _ in range(exponent):
+            result += base
+        return result
+
+def user_input() -> int:
+    print("Multiplication and Exponent Calculator\nChoose option 1 for Multiplication"
+          "\nChoose option 2 for Exponentiation\nChoose option 3 to Exit")
+    return int(input())
+
+while True:
+    try:
+        operation = user_input()
+
+        match operation:
+            case 1:
+                a = int(input("Enter an operand: "))
+                b = int(input("Enter another operand: "))
+                result = multiplication(a,b)
+                print(f"{a} x {b} = {result}")
+                break
+
+            case 2:
+                base = int(input("Enter an operand: "))
+                exponent = int(input("Enter another operand: "))
+                result = exponentiate(base, exponent)
+                print(f"{base}^{exponent} = {result}")
+                break
+
+            case 3:
+                print("Closing the Calculator...")
+                break
+
+            case _:
+                print("Invalid Choice")
+                break
+
+
+    except ValueError:
+        print("Invalid Choice)")
+
+
+
+
