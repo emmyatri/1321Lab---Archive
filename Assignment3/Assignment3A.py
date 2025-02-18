dad00db46552fb289d53c8a972f2906495dd6039
emmyatri <amelia.ellingson@gmail.com>
2025-02-18 13:59:47 -0500

    update assignment3A

--- Assignment3/Assignment3A.py
+++ Assignment3/Assignment3A.py
@@ -5,3 +5,21 @@
 #Name: Amelia Ellingson
 #Lab#: Assignment3A
 
+
+odd_num = int(input("Enter an odd number for the size of the diamond: "))
+
+if odd_num % 2 == 0:
+    print(f"Size must be an odd number; we will increase it to {odd_num + 1}")
+    odd_num += 1
+
+number = 0
+
+for i in range (number//2 + 1):
+    for j in range(number//2 - i):
+        print(" ", end="")
+
+    for j in range(2*i + 1):
+        print(number, end="")
+        number = (number + 1)
+
+        print()
