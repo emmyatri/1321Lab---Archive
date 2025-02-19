7d4ec9bd70700fad87d20db7337f4e2f183c489f
emmyatri <amelia.ellingson@gmail.com>
2025-02-19 18:29:27 -0500

    Assignment 3b

--- Assignment3/Assignment3B.py
+++ Assignment3/Assignment3B.py
@@ -4,3 +4,25 @@
 #Instructor: Roshni Satish
 #Name: Amelia Ellingson
 #Lab#: Assignment3B
+
+print("[Character Frequencies]")
+
+#user input
+word_input = str(input("Enter a string: "))
+#user input set to lowercase w/ removed spaces
+letter_count = word_input.lower().replace(" ", "")
+
+original = letter_count
+
+while letter_count:
+    current_character = letter_count[0]
+
+    original_length = 0
+    new_length = 0
+
+    for current_character in letter_count:
+        original_length += 1
+
+    text_after = letter_count
+
+
