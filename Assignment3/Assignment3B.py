a7356e27e8d53222043512aea16da4d314cda0ed
emmyatri <amelia.ellingson@gmail.com>
2025-02-23 15:53:10 -0500

    Update Assignment3B.py

--- Assignment3/Assignment3B.py
+++ Assignment3/Assignment3B.py
@@ -10,10 +10,40 @@ print("[Character Frequencies]")
 #user input
 string = (input("Enter a string: "))
 
+#remove spaces and account for case
 string = string.lower()
 string = string.replace(" ", "")
 
-print(string)
+#reference string
+original_string = string
+
+#character count loop
+while string:
+    #first character
+    current_char = string[0]
+
+    #count occurences by comparing the length before and after replacement
+    original_length = 0
+    new_length = 0
+
+    for char in string:
+        original_length += 1
+
+    post_string = string.replace(current_char, "")
+
+    for char in post_string:
+        new_length += 1
+
+    freq = original_length - new_length
+
+    if freq == 1:
+        print(f"{current_char} appears {freq} time.")
+    else:
+        print(f"{current_char} appears {freq} times.")
+
+    string = post_string
+
+
 
 
 
