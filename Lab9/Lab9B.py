849944e18352cc2dad830d19d72ce49671398a1a
emmyatri <amelia.ellingson@gmail.com>
2025-03-17 13:58:19 -0400

    Update Lab9B.py

--- Lab9/Lab9B.py
+++ Lab9/Lab9B.py
@@ -6,9 +6,61 @@
 #Lab#: Lab9B
 
 
+def register(users):
+    print("\n[Register]")
+    username = input("Username: ")
+    password = input("Password: ")
+    users[username] = password
+    print("User successfully added!")
+
+def change_password(users, username):
+    new_password = input("Password: ")
+    users[username] = new_password
+    print("Password updated successfully!")
 
 def main():
     users = {}
+
+    while True:
+        print("\nChoose an option \n1 - Login \n2 - Registration \nE - Exit")
+        choice = input()
+
+        if choice == "1":
+            print()
+            print("[Login]")
+            username = input("Username: ")
+            password = input("Password: ")
+
+            if username in users and users[username] == password:
+                print("Success!")
+
+                while True:
+                    print("\nChoose an option \n3 - Change Password \n4 - Logout \nE - Exit")
+                    login_choice = input()
+
+                    if login_choice == '3':
+                        change_password(users, username)
+
+                    elif login_choice == '4':
+                        print("\nLogging out...")
+                        break
+                    elif login_choice == 'E':
+                        print("\nTerminating...")
+                        break
+                    else:
+                        print("Invalid choice.")
+            else:
+                print("Incorrect username/password!")
+
+        elif choice == '2':
+            register(users)
+
+        elif choice == 'E':
+            print("Terminating...")
+            break
+        else:
+            print("Invalid choice.")
+
 
 
 if __name__ == "__main__":
