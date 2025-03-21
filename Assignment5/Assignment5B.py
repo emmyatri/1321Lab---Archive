85a30be0227d2d0ed00bdda6c2a660ff664a7cb1
emmyatri <amelia.ellingson@gmail.com>
2025-03-21 19:53:38 -0400

    assignment5

--- Assignment5/Assignment5B.py
+++ Assignment5/Assignment5B.py
@@ -5,11 +5,48 @@
 #Name: Amelia Ellingson
 #Lab#: Assignment5B
 
-def main():
-    board = []
-    width = input("Enter the width of the grid: ")
-    height = input("Enter the height of the grid: ")
+import random
 
+def game_board (width, height):
+    board = []
+    numberOfUndiscoveredTreasures = 0
+
+    first_row = []
+    for i in range(width):
+        rand_num = random.random()
+        if rand_num >= 0.7:
+            first_row.append("T")
+            numberOfUndiscoveredTreasures += 1
+        else:
+            first_row.append("O")
+    board.append(first_row)
+
+    for i in range(height - 1):
+        row = []
+        for j in range(width):
+            rand_num = random.random()
+            if rand_num >= 0.7:
+                row.append("T")
+                numberOfUndiscoveredTreasures += 1
+            else:
+                row.append("O")
+        board.append(row)
+
+    return board, numberOfUndiscoveredTreasures
+
+def print_board(board, show_treasure=False):
+    for row in board:
+        for cell in row:
+            if not show_treasure and cell == "T":
+                print('O', end="")
+            else:
+                print(cell, end="")
+        print()
+
+def main():
+    width = int(input("Width: "))
+    height = int(input("Height: "))
+    board, treasures = game_board(width, height)
 
 if __name__ == "__main__":
     main()
